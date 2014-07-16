e1568578aa6995789ba3e77f4679ac6dc1b63ae2
Gauthier Delmee <gauthier.delmee@cyarasolutions.com>
2014-07-16 15:47:58 +1000

    Initial Commit

---
+++ Git Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git Test
+//
+//  Created by Gauthier Delmee on 16/07/2014.
+//  Copyright (c) 2014 Expectancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Gauthier Delmee on 16/07/2014.
+//  Copyright (c) 2014 Expectancy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
