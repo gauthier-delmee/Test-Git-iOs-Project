5b18f810214a07df655ab4bc23ace789c63bda23
Gauthier Delmee <gauthier.delmee@cyarasolutions.com>
2014-07-16 16:01:37 +1000

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Gauthier Delmee on 16/07/2014.
+//  Copyright (c) 2014 Expectancy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Gauthier Delmee on 16/07/2014.
+//  Copyright (c) 2014 Expectancy. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
